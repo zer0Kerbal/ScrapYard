588118f7e406de88a125def0d9d4165adef803f2
severedsolo <severedsolo@users.noreply.github.com>
2019-04-04 22:10:51 +0100

    Potentially fix KRASH simulations messing with builds

--- ScrapYard/PartTracker.cs
+++ ScrapYard/PartTracker.cs
@@ -75,12 +75,14 @@ namespace ScrapYard
                 }
             }
 
-            //Increment the build tracker
-            foreach (var partPair in uniqueParts)
+            if (!KRASHWrapper.simulationActive())
             {
-                addBuild(partPair.Key, partPair.Value);
+                //Increment the build tracker
+                foreach (var partPair in uniqueParts)
+                {
+                    addBuild(partPair.Key, partPair.Value);
+                }
             }
-
             ScrapYardEvents.OnSYTrackerUpdated.Fire(uniqueParts.Keys);
         }
 
@@ -117,11 +119,15 @@ namespace ScrapYard
                 }
             }
 
-            //Increment the build tracker
-            foreach (var partPair in uniqueParts)
+            if (!KRASHWrapper.simulationActive())
             {
-                addBuild(partPair.Key, partPair.Value);
+                //Increment the build tracker
+                foreach (var partPair in uniqueParts)
+                {
+                    addBuild(partPair.Key, partPair.Value);
+                }
             }
+
             ScrapYardEvents.OnSYTrackerUpdated.Fire(uniqueParts.Keys);
         }
         #endregion Add
@@ -235,7 +241,7 @@ namespace ScrapYard
             InventoryPart found = _buildTracker.Keys.FirstOrDefault(ip => ip.IsSameAs(source, _strictness));
             if (found != null)
             {
-                _buildTracker[found].buildsTotal++; //increment it
+                    _buildTracker[found].buildsTotal++; //increment it
             }
             else
             {
